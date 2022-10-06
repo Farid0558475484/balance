748b3e55c598420f828ac45e8a938eaf79d891bb
Farid0558475484 <faridmh@code.edu.az>
2022-10-06 18:43:29 +0400

    first commit

---
+++ Js/index.js
@@ -0,0 +1,79 @@
+const personFarid = {
+  firstname: "Farid",
+  lastname: "Hacizade",
+  salary: 700,
+};
+const personRovsen = {
+  firstname: "Rovsen",
+  lastname: "Xelilov",
+  salary: 900,
+};
+const personTunar = {
+  firstname: "Tunar",
+  lastname: "Namazov",
+  salary: 500,
+};
+const personElnar = {
+  firstname: "Elnar",
+  lastname: "Mamedov",
+  salary: 1100,
+};
+const personElmar = {
+  firstname: "Elmar",
+  lastname: "Haciyev",
+  salary: 1300,
+};
+
+// let multiply = 0;
+const work = [personFarid, personRovsen, personTunar, personElnar, personElmar];
+
+// function balans(item) {
+//   multiply += item.salary / work;
+// }
+// function myforeach(arr) {
+//     for (let i = 0; index < salary.length; i++) {
+//         const element = salary[i];
+
+//     }
+
+// }
+function person() {
+    // const work = [personFarid, personRovsen, personTunar, personElnar, personElmar];
+    console.log([personFarid, personRovsen, personTunar, personElnar, personElmar]);
+}
+person();
+
+
+// person.balance=work;
+// console.log(person.balance);
+
+// function balance(salary) {
+//     return function (item) {
+//         return salary ;
+//     };
+// }
+
+// balance();
+// console.log(balance);
+
+
+
+// console.log(work);
+
+
+
+
+// function balance(salary) {
+    
+//     // multiply += item.salary / work;
+//     for (let i = 0; i < salary.length; i++) {
+//         const element = salary[i];
+        
+       
+//     }
+    
+// }
+
+
+
+// console.log("Ortalama Maas:" ,balance);
